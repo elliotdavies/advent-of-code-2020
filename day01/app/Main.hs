c26c2532fa3106b35788dee596b7bedcff61b930
Elliot Davies <elliot.a.davies@gmail.com>
2020-12-31 12:49:49 +0000

    Improve solution (thanks @i-am-tom)

--- day01/app/Main.hs
+++ day01/app/Main.hs
@@ -11,39 +11,38 @@ module Main where
 
 import Data.Proxy (Proxy(..))
 import GHC.TypeNats
-import Prelude (IO, undefined)
+import Prelude (IO, Ordering(..), undefined)
+
+type NatPair = (Nat, Nat)
 
 -- Append lists
-type family (xs :: [k]) ++ (ys :: [k]) where
+type family (xs :: [k]) ++ (ys :: [k]) :: [k] where
   '[] ++ ys = ys
   (x ': xs) ++ ys = x ': (xs ++ ys)
 
--- Lookup within lists
-type family IndexOf (x :: k) (xs :: [k]) :: Nat where
-  IndexOf x (x ': xs) = 0
-  IndexOf x (y ': xs) = 1 + (IndexOf x xs)
-
--- Index into lists
-type family (xs :: [k]) !! (n :: Nat) where
-  (x ': xs) !! 0 = x
-  (x ': xs) !! n = xs !! (n - 1)
-
 -- map (x,) xs
-type family Tuples (x :: k) (xs :: [k]) where
+type family Tuples (x :: k) (xs :: [k']) :: [(k, k')] where
   Tuples x '[] = '[]
   Tuples x (y ': ys) = '(x, y) ': Tuples x ys
 
-type family UniquePairs (xs :: [k]) :: [(Nat, Nat)] where
+type family UniquePairs (xs :: [k]) :: [NatPair] where
   UniquePairs (x ': xs) = (Tuples x xs) ++ (UniquePairs xs)
 
-type family PairSum (p :: (Nat, Nat)) :: Nat where
+type family PairSum (p :: NatPair) :: Nat where
   PairSum '(x, y) = x + y
 
-type family PairMult (p :: (Nat, Nat)) :: Nat where
+type family PairMult (p :: NatPair) :: Nat where
   PairMult '(x, y) = x * y
 
-type family Sums (ps :: [(Nat, Nat)]) :: [Nat] where
+type family Sums (ps :: [NatPair]) :: [Nat] where
   Sums (p ': ps) = (PairSum p) ': (Sums ps)
+
+type family FindPair_ (p :: NatPair) (ps :: [NatPair]) (o :: Ordering) :: NatPair where
+  FindPair_ p ps 'EQ = p
+  FindPair_ p ps _   = FindPair ps
+
+type family FindPair (ps :: [NatPair]) :: NatPair where
+  FindPair (p ': ps) = FindPair_ p ps (CmpNat (PairSum p) 2020)
 
 -- Sample input
 type Inputs =
@@ -57,9 +56,8 @@ type Inputs =
 
 -- `:type result` in GHCi
 result ::
-  ( pairs ~ UniquePairs Inputs
-  , idx ~ IndexOf 2020 (Sums pairs)
-  , pair ~ (pairs !! idx)
+  ( pairs  ~ UniquePairs Inputs
+  , pair   ~ FindPair pairs
   , result ~ PairMult pair
   )
   => Proxy result
